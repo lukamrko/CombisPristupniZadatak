5cb854537f29c9f06383d6706d2c43c525d29c16
lukamrko <lukamrko@gmail.com>
2022-05-08 17:16:17 +0200

    To make sure my application is web-application I will be switching frontend to Angular

--- RealFrontEnd/JSskripta.js
+++ RealFrontEnd/JSskripta.js
@@ -1,4 +1,33 @@
 $(document).ready(function ()
 {
+    $("#unos").on("click", function ()
+    {
+        console.log("kliknuto!");
+        var firstname=$("#inputFirstName").val();
+        var lastname=$("#inputFirstName").val();
+        var email=$("#inputEmail").val();
+        var mobliePhone=$("#inputMobilePhone").val();
+        var address=$("#inputAddress").val();
+        var person = new Person(firstname, lastname, email, mobliePhone, address);
+        $.post('https://localhost:7265/People', { myData: person },
+            function (data, status, xhr)
+        {   
+            alert('status: ' + status + ', data: ' + data.responseData);
+        },
+       'json'); 
+    });
 
+});
+
+
+class Person
+{
+    constructor(firstname, lastname, email, mobliePhone, address)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.mobliePhone = mobliePhone;
+        this.address = address;
+    }
 }
